5ef560e4d0af732634640218096b052d73bfa3d5
Abdul-Rahman-E <abdulrahimaneassak@gmail.com>
2026-01-06 13:51:51 +0530

    Implement invalid command handling

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    sys.stdout.flush()
+    user_input = sys.stdin.readline().strip()
+    print(f"{user_input}: command not found")
     pass
 
 
