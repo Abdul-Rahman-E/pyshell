ec4c296430044f87f5e59e9e8154eb2bd7519869
Abdul-Rahman-E <abdulrahimaneassak@gmail.com>
2026-01-06 14:06:47 +0530

    Implement REPL and exit

--- app/main.py
+++ app/main.py
@@ -2,12 +2,16 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    user_input = sys.stdin.readline().strip()
-    print(f"{user_input}: command not found")
-    pass
+    try:
+        while True:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
+            user_input = sys.stdin.readline().strip()
+            print(f"{user_input}: command not found")
+    
+    except KeyboardInterrupt:
+        print("\nExiting shell.")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
