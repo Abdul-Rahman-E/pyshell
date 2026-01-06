d1d0a0519cdbc13a0fa1a34689944757f3e16a0e
Abdul-Rahman-E <abdulrahimaneassak@gmail.com>
2026-01-06 14:20:16 +0530

    Implement evaluate command function for exit and invalid commands

--- app/main.py
+++ app/main.py
@@ -1,5 +1,26 @@
 import sys
 
+def evaluateCommand(command: str):
+
+    def exitCommand():
+        print("Exiting shell.")
+        sys.exit(0)
+
+    def commandNotFound(cmd):
+        print(f"{cmd}: command not found")
+
+    commandDict = {
+        "exit": exitCommand,
+    }
+
+    if not command: 
+        return
+
+    if command not in commandDict:
+        commandNotFound(command)
+        return
+
+    commandDict[command]()
 
 def main():
     try:
@@ -7,12 +28,11 @@ def main():
             sys.stdout.write("$ ")
             sys.stdout.flush()
             user_input = sys.stdin.readline().strip()
-            print(f"{user_input}: command not found")
-    
+            evaluateCommand(user_input)
+
     except KeyboardInterrupt:
         print("\nExiting shell.")
         sys.exit(0)
-
 
 if __name__ == "__main__":
     main()
